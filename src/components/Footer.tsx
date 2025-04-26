da5460846402ff3cfd38a9c57c0556e460ade559
Arijeet Das <arijeetdas0523@gmail.com>
2025-04-26 11:34:41 +0000

    add margin top of footer of 50 px

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,35 +2,36 @@
 
 import React from 'react';
 import { Icons } from '@/components/icons';
+import Link from 'next/link'; // Import Link
 
 const Footer = () => {
   return (
-    <nav className="bg-[#152226] p-4 fixed bottom-0 left-0 w-full border-t border-gray-700 dark:border-gray-800">
+    <nav className="bg-[#152226] p-4 fixed bottom-0 left-0 w-full border-t border-gray-700 dark:border-gray-800 mt-[50px]">
       <ul className="flex justify-between items-center">
         <li className="flex flex-col items-center">
-          <a href="/" className="flex flex-col items-center">
+          <Link href="/" className="flex flex-col items-center">
             <Icons.home className="w-6 h-6 mb-1 text-gray-400 dark:text-gray-300" />
             <span className="text-xs text-gray-400 dark:text-gray-300">My Booksy</span>
-          </a>
+          </Link>
         </li>
         <li className="flex flex-col items-center">
-          <a href="/search" className="flex flex-col items-center">
+          <Link href="/search" className="flex flex-col items-center">
             <Icons.search className="w-6 h-6 mb-1 text-gray-400 dark:text-gray-300" />
             <span className="text-xs text-gray-400 dark:text-gray-300">Search</span>
-          </a>
+          </Link>
         </li>
         <li className="flex flex-col items-center">
-          <a href="#" className="flex flex-col items-center">
+          <Link href="#" className="flex flex-col items-center">
             <Icons.calendar className="w-6 h-6 mb-1 text-gray-400 dark:text-gray-300" />
             <span className="text-xs text-gray-400 dark:text-gray-300">Calendar</span>
-          </a>
+          </Link>
         </li>
         <li className="flex flex-col items-center">
           {/* Updated href to point to the profile page */}
-          <a href="/profile" className="flex flex-col items-center">
+          <Link href="/profile" className="flex flex-col items-center">
             <Icons.user className="w-6 h-6 mb-1 text-gray-400 dark:text-gray-300" />
             <span className="text-xs text-gray-400 dark:text-gray-300">Profile</span>
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
