2fd3c364914c6141e0d240adb2d0e2f145e8b77a
Arijeet Das <arijeetdas0523@gmail.com>
2025-05-01 18:09:12 +0000

    images changed

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,49 +5,49 @@ export const serviceCategories = [
   {
     id: 'dental', // Use descriptive IDs
     name: 'Dental & Orthodontics',
-    image: 'https://picsum.photos/200/300',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR0I9prkX7oEHn-jTrsVsFI3S3Kzdr5mayOg&s',
     icon: Icons.tooth,
     "data-ai-hint": "dental orthodontics",
   },
   {
     id: 'fitness',
     name: 'Health & Fitness',
-    image: 'https://picsum.photos/200/301',
+    image: 'https://i0.wp.com/goldsgym.in/wp-content/uploads/2023/12/compress-strong-man-training-gym-min-scaled.jpg?fit=2560%2C1707&ssl=1',
     icon: Icons.dumbbell,
     "data-ai-hint": "health fitness gym workout",
   },
   {
     id: 'professional',
     name: 'Professional Services',
-    image: 'https://picsum.photos/200/302',
+    image: 'https://blog.ipleaders.in/wp-content/uploads/2020/01/Professional-Services.jpg',
     icon: Icons.briefcase,
     "data-ai-hint": "professional business services",
   },
    {
     id: 'barber',
     name: 'Barbershop',
-    image: 'https://picsum.photos/200/304',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStor9vEAXB_4QZjTmJnggJ1H85KlvuX2ZsQw&s',
     icon: Icons.scissors,
      "data-ai-hint": "barbershop haircut",
   },
    {
     id: 'beauty',
     name: 'Beauty Salon',
-    image: 'https://picsum.photos/200/305',
+    image: 'https://media.istockphoto.com/id/1394936524/photo/young-woman-on-manicure-treatment-at-beauty-salon.jpg?s=612x612&w=0&k=20&c=c1PDGyhDVL7LEJQks__JZPh4_TN0kliSPowhrTROzmg=',
     icon: Icons.scissors, // Reusing scissors or find a better alternative if available
     "data-ai-hint": "beauty salon hair makeup",
   },
    {
     id: 'massage',
     name: 'Massage Therapy',
-    image: 'https://picsum.photos/200/306',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz5Az-ggXZuf9uF8xLMmy-mtqjlW6lOxN_Ig&s',
     icon: Icons.massage, // Use the updated massage icon
     "data-ai-hint": "massage therapy spa relaxation",
   },
   {
     id: 'other',
     name: 'Other',
-    image: 'https://picsum.photos/200/303',
+    image: 'https://www.shutterstock.com/image-photo/two-hands-touching-each-other-260nw-2458989061.jpg',
     icon: Icons.more,
      "data-ai-hint": "various services",
   },
